d62aac390d1073e09fc2da4abfe25fd264bb65b2
MoraisBandeira <anderson.moraisbandeira@outlook.com>
2024-04-12 00:37:38 -0300

    ♻️ refector:

--- src/core/usuario/model/Usuario.ts
+++ src/core/usuario/model/Usuario.ts
@@ -8,36 +8,67 @@ export  default interface IUsuario{
 }
 
 export class Usuario{
+    private readonly senha: Senha
+    private readonly nome: Nome
+    private readonly email: Email
     private readonly id?: number | undefined
-    private readonly nome: Nome|Error
-    private readonly email: Email|Error
-    private readonly senha: string
     private readonly created_at?: Date | undefined
     private readonly updated_at?: Date | undefined
     
     private constructor(usuario:IUsuario){
         this.id = usuario?.id;
-        this.nome = Nome.create(usuario.nome);
-        this.email = Email.create(usuario.email);
-        this.senha = usuario.senha;
         this.created_at=usuario?.created_at;
         this.updated_at=usuario?.updated_at;
+        this.nome = Nome.create(usuario.nome);
+        this.senha = Senha.create(usuario.senha);
+        this.email = Email.create(usuario.email);
     }
 
     static create(usuario:IUsuario):Usuario{
         return new Usuario(usuario)
     }
+
+    get getNome():Nome|Error{
+        return this.nome;
+    }
+}
+export abstract class Validate {
+    static isValidMail(email:string):boolean{
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+    static isValidHost(email:string):boolean{
+        const HostPermitidos:string[] = ['hemoce.ce.gov.br','gmail.com'];
+        const partesEmail = email.split("@");
+        if (partesEmail.length !== 2) {
+            return false;
+        }
+        const dominioEmail = partesEmail[1];
+        return HostPermitidos.includes(dominioEmail);
+    }
+    static detectarCaracteresEspeciais(nome: string): boolean {
+        const regex = /[^\w\sÀ-ÿ'-]/;
+        return !regex.test(nome);
+    }
+    static  detectarNumeros(texto: string): boolean {
+        const regex = /\d/;
+        return !regex.test(texto);
+    }
 }
 
-export class Nome {
+export class Nome extends Validate{
     private readonly nome:string;
     
     private constructor(nome:string){
+        super()
         this.nome = nome;
         Object.freeze(this);
     }
-    static create(nome:string):Nome|Error{
-        return new Nome(nome);
+    static create(nome:string):Nome{
+        if(this.detectarCaracteresEspeciais(nome),this.detectarNumeros(nome)){
+            return new Nome(nome);
+        }
+        throw Error('Nome Invalido');
     }
     
     get value ():string{
@@ -45,33 +76,38 @@ export class Nome {
     }
 }
 
-export class Email {
+export class Email extends Validate{
     private readonly email:string;
-    private HostPermitidos:string[] = ['hemoce.ce.gov.br','gmail.com'];
     
     private constructor(email:string){
+        super();
         this.email = email;
         Object.freeze(this);
     }
-    static create(email:string):Email|Error{
-        if( this.isValidMail(email) && this.IsValidHost(email) ){
+    static create(email:string):Email{
+        if( this.isValidMail(email) && this.isValidHost(email) ){
             return new Email(email)
         }
-        return new Error('invalid param')
+        throw new Error('Email fora do padrão')
     }
-
-    static isValidMail(email:string):boolean{
-        return false
-    }
-
-    static IsValidHost(email:string):boolean{
-        return true;
-    }
-    
     get value ():string{
         return this.email
     }
+}
 
+export class Senha {
+    private readonly senha:string;
+
+    private constructor(senha:string){
+        this.senha = senha;
+    }
+
+    static create(senha:string):Senha{
+        return new Senha(senha);
+    }
+    get value():string{
+        return this.senha;
+    }
 }
 
 
