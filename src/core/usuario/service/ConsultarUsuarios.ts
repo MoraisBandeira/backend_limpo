0ddbbf9db18423bc6e8374e9ce7e55d81edb5630
MoraisBandeira <anderson.moraisbandeira@outlook.com>
2024-04-11 21:21:42 -0300

    ♻️ refactor: DTOs

--- src/core/usuario/service/ConsultarUsuarios.ts
+++ src/core/usuario/service/ConsultarUsuarios.ts
@@ -1,11 +1,23 @@
 import UseCase from "../../shared/UseCase";
 import RepositoryUsuario from "../model/RepositorioUsuario";
 import Usuario from "../model/Usuario";
-
-export default class ConsultarUsuarios implements UseCase<void,Usuario[]>{
+type UsuarioDTO={
+    id:number|undefined
+    nome: string
+    email: string
+    created_at:Date|undefined
+}
+export default class ConsultarUsuarios implements UseCase<void,UsuarioDTO[]>{
     constructor(readonly repositorio:RepositoryUsuario){}
-    async executar(): Promise<Usuario[]> {
-        return await this.repositorio.buscarTodos();
+    async executar(): Promise<UsuarioDTO[]> {
+        const usuarios = await this.repositorio.buscarTodos();
+        return usuarios.map(user=>{
+            return{
+            id:user.id,
+            nome: user.nome,
+            email: user.email,
+            created_at:user.created_at
+        }})
     }
 
 }
--- src/core/usuario/service/RegistrarUsuario.ts
+++ src/core/usuario/service/RegistrarUsuario.ts
@@ -6,15 +6,27 @@ type Entrada = {
     email: string
     senha: string
 }
-
-export default class RegistrarUsuario implements UseCase<Entrada,Usuario>{
+type Saida={
+    id:number|undefined
+    nome: string
+    email: string
+    created_at:Date|undefined
+}
+export default class RegistrarUsuario implements UseCase<Entrada,Saida>{
     constructor(private readonly repositorio:RepositoryUsuario){}
-    async executar(input: Entrada): Promise<Usuario> {
+    async executar(input: Entrada): Promise<Saida> {
        const {nome,email,senha} = input
        const usuarioExistente = await this.repositorio.consutarPorEmail(email);
        if(usuarioExistente){
         throw new Error("usuario já existe")
        }
-         return this.repositorio.registrar({nome,email,senha})
+         const usuario = await this.repositorio.registrar({nome,email,senha})
+
+         return {
+            id:usuario.id,
+            nome: usuario.nome,
+            email: usuario.email,
+            created_at:usuario.created_at
+         } as Saida
     }
 }
