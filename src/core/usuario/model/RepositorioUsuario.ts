aacc054a74698bffcfec12478375c0635d9bb432
anderson.morais <anderson.morais@hemoce.ce.gov.brgit config --global user.email anderson.morais@hemoce.ce.gov.br>
2024-04-12 14:42:16 -0300

    ♻️  refactor: aplicar interface

--- src/core/usuario/model/RepositorioUsuario.ts
+++ src/core/usuario/model/RepositorioUsuario.ts
@@ -1,10 +1,11 @@
-import Usuario from "./Usuario";
+import IUsuario from "./Usuario";
 
 export default interface RepositoryUsuario{
-    registrar(usuario:Usuario):Promise<Usuario>
-    buscarTodos():Promise<Usuario[]>
-    atualizar():Promise<Usuario | null>
-    consutarPorEmail(email:string):Promise<Usuario | null>
-    remover():Promise<Usuario | null>
-    apagar():Promise<Usuario | null>
+    registrar(usuario:IUsuario):Promise<IUsuario>
+    buscarUsuario(id:number):Promise<IUsuario>
+    buscarTodos():Promise<IUsuario[]>
+    atualizar():Promise<IUsuario | null>
+    consultarPorEmail(email:string):Promise<IUsuario | null>
+    remover():Promise<IUsuario | null>
+    apagar():Promise<IUsuario | null>
 }
